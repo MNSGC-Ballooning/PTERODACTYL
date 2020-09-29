04afc9621cfaae289807facc7fb678567c8fed99
Andrew Van Gerpen <47224205+AndrewVanGerpen@users.noreply.github.com>
2020-09-28 20:31:27 -0500

    first commit w new name

---
+++ PTERODACTYL/PTERODACTYL.ino
@@ -0,0 +1,229 @@
+#define fixLED 26                 // LED to indicate GPS fix
+#define ppodLED 24
+#define xbeeLED 27                // LED to indicate xbee communication
+#define sdLED 25
+#define serialBAUD 9600           // when using arduino serial monitor, make sure baud rate is set to this same value
+
+#define ppodSwitchPin 30
+#define magnetSwitchPin 31
+#define commsSwitchPin 32
+#define setAltSwitch 28
+#define altSwitch 29
+#define baroSwitchPin 9
+#define id1SwitchPin 20
+#define id2SwitchPin 21
+#define id3SwitchPin 22
+#define id4SwitchPin 23
+#define pullBeforeFlightPin 16
+
+int rfd900 = 1; // set true if you want this thing to operate with a rfd900 on serialX (declare above)
+int ppod = 1; // set true if you want this thing to operate as ppod flight computer
+int magnetBootup = 1; // set true if you want to activate flight by waving a magnet over the IMU
+int setAltVal = 1;
+int altVal = 1;
+int baroOn = 1;
+int id1On = 1;
+int id2On = 1;
+int id3On = 1;
+int id4On = 1;
+int pullOn = 1;
+
+float cutTime = 70*60000.0; // x (minutes) * 60000;
+float cutAltitude = 10000; // 50,000 ft ASL cut!!
+bool smartRelease = false; // Releases the smart unit to deploy cubes
+int smartReleasePosition = 170; // the angle in degrees for smart release. Will vary based on smart unit
+int smartInitialPosition = 0; // the angle in degrees for smart initial position. Will vary based on smart unit
+bool smartReleaseTransmission = true; // This ensures only one message is relayed to the ground after release has occurred.
+String commandMessage; // Appends a message stating the radio deployed the cubes if that happens
+
+String header = "Date, Time, Lat, Lon, Alt(ft), AltEst(ft), intT(F), extT(F), msTemp(F), analogPress(PSI), msPressure(PSI), time since bootup (sec), Recent Radio Traffic, magnetometer x, magnetometer y, magnetometer z, accelerometer x, accelerometer y, accelerometer z, gyroscope x, gyroscope y, gyroscope z";
+unsigned long int dataTimer = 0;
+unsigned long int dataTimerIMU = 0;
+unsigned long int ppodOffset = 0;
+int dataRate = 1000; // 1000 millis = 1 second
+int dataRateIMU = 250; // 250 millis = .25 seconds
+int analogResolutionBits = 14;
+int analogResolutionVals = pow(2,analogResolutionBits);
+float pressureBoundary1;
+float pressureBoundary2;
+float pressureBoundary3;
+float pressureOnePSI;
+float msPressure = -1.0;
+float msTemperature = -1.0;
+float altitudeFt = -1.0;
+unsigned long int fixTimer = 0;
+bool fix = false; // determines if the GPS has a lock
+
+///////////////////// Sensor Global Variables /////////////////////////////
+
+float dallasOneF;
+float thermistorInt;
+float thermistorExt;
+float magnetometer[3]; // {x, y, z}
+float accelerometer[3]; // {x, y, z}
+float gyroscope[3]; // {x, y, z}
+
+float altitudeFtGPS;
+float latitudeGPS;
+float longitudeGPS;
+String data;
+String groundData;
+String IMUdata;
+
+String exclamation = "!"; // this needs to be at the end of every 
+bool commandRelease = false;
+String groundCommand;
+String xbeeID = "UMN0";
+bool xbeeAlternation = false; // this will allow the xbee to alternate between sending data to MOC SOC and data via MOC SOC to the ground
+unsigned long int xbeeTimer = 0;
+unsigned long int xbeeRate = 5000; // 10000 millis = 10 seconds
+String xbeeMessage; // This saves all xbee transmissions and appends them to the data string
+
+void setup() {
+  
+  Serial.begin(serialBAUD); //define baud rate in variable decleration above
+  Serial.println("Serial online");
+  pinMode(fixLED,OUTPUT); 
+  pinMode(xbeeLED,OUTPUT);
+  pinMode(sdLED,OUTPUT); 
+  pinMode(ppodLED,OUTPUT);
+  pinMode(13,OUTPUT);
+  pinMode(setAltSwitch, INPUT_PULLUP);
+  pinMode(altSwitch, INPUT_PULLUP);
+  checkSwitches(); // slide and button switch statuss
+
+  if(id1On==0)xbeeID="UMN1";
+  if(id2On==0)xbeeID="UMN2";
+  if(id3On==0)xbeeID="UMN3";
+  if(id4On==0)xbeeID="UMN4";
+  if(rfd900==0)xbeeID = "COMM";
+  if(ppod==0)xbeeID = "PPOD";
+
+  Serial.print("starting OLED setup... ");
+  oledSetup();
+  Serial.println("OLED setup complete");
+  
+  Serial.print("starting IMU setup... ");
+  imuSetup();
+  updateIMU();
+  Serial.println("IMU setup complete");
+
+  Serial.print("starting Altimeter setup... ");
+  if(baroOn==1)msSetup();
+  else{ updateOled("MS5611\nOffline.");
+    delay(2000);
+  }
+  Serial.println("Altimeter setup complete");
+
+  Serial.print("starting SD setup... ");
+  sdSetup();
+  Serial.println("SD setup complete");
+
+  Serial.print("starting xbee setup... ");
+  xbeeSetup();
+  Serial.println("xbee setup complete");
+
+  Serial.print("starting ublox setup... ");
+  ubloxSetup();
+  Serial.println("ublox setup complete");
+  
+  pressureToAltitudeSetup();
+  if(ppod==0) ppodSetup();
+  if(rfd900==0) rfd900Setup();
+  logData(header);
+  if(pullOn==0) pullPin();
+}
+
+void loop() {
+  updateData(); 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// Functions  ////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////
+
+void pressureToAltitudeSetup()
+{
+  float h1 = 36152.0;
+  float h2 = 82345.0;
+  float T1 = 59-.00356*h1;
+  float T2 = -70;
+  float T3 = -205.05 + .00164*h2;
+  pressureBoundary1 = (2116 * pow(((T1+459.7)/518.6),5.256));
+  pressureBoundary2 = (473.1*exp(1.73-.000048*h2)); // does exp function work??
+  pressureBoundary3 = (51.97*pow(((T3 + 459.7)/389.98),-11.388));
+}
+
+void pressureToAltitude(){
+  //float pressurePSF = (pressureOnePSI*144);
+  float pressurePSF = (msPressure*144);
+  
+  float altFt = -100.0;
+  //UNCOMMENT WHEN RELIABLE PRESSURE SENSORS ARE ON BOARD
+  if (pressurePSF > pressureBoundary1)// altitude is less than 36,152 ft ASL
+  {
+    altFt = (459.7+59-518.6*pow((pressurePSF/2116),(1/5.256)))/.00356;
+  }
+  else if (pressurePSF <= pressureBoundary1 && pressurePSF > pressureBoundary2) // altitude is between 36,152 and 82,345 ft ASL
+  {
+    altFt = (1.73-log(pressurePSF/473.1))/.000048;
+  }
+  else if (pressurePSF <= pressureBoundary2)// altitude is greater than 82,345 ft ASL
+  {
+    altFt = (459.7-205.5-389.98*pow((pressurePSF/51.97),(1/-11.388)))/-.00164;
+  }
+  else{altFt = -1.0;}
+  
+  altitudeFt = altFt;
+  if(baroOn==0)altitudeFt = -1.0;
+}
+
+void updateData(){
+  updateUblox();
+  updateXbee();
+
+  if(millis() - dataTimerIMU > dataRateIMU){
+    dataTimerIMU = millis();
+    updateIMU();
+  }
+  if(millis() - dataTimer > dataRate){
+    dataTimer = millis();
+    if(fix == true){
+      digitalWrite(fixLED,HIGH);
+    }
+    digitalWrite(sdLED,HIGH);
+    delay(30);
+    digitalWrite(sdLED,LOW);
+    digitalWrite(fixLED,LOW);
+    pressureToAltitude();
+    updateThermistor();
+    if(baroOn==1) updateMS(); //Not every payload has one
+    updatePressure();
+    updateIMU();
+    updateSmart();
+    updateDataStrings();
+    xbeeMessage="";
+  }        
+}
+
+void checkSwitches(){
+  pinMode(ppodSwitchPin, INPUT_PULLUP);
+  pinMode(magnetSwitchPin, INPUT_PULLUP);
+  pinMode(commsSwitchPin, INPUT_PULLUP);
+  pinMode(baroSwitchPin, INPUT_PULLUP);
+  pinMode(id1SwitchPin, INPUT_PULLUP);
+  pinMode(id2SwitchPin, INPUT_PULLUP);
+  pinMode(id3SwitchPin, INPUT_PULLUP);
+  pinMode(id4SwitchPin, INPUT_PULLUP);
+  pinMode(pullBeforeFlightPin, INPUT_PULLUP);
+
+  ppod = digitalRead(ppodSwitchPin);
+  rfd900 = digitalRead(commsSwitchPin);
+  magnetBootup = digitalRead(magnetSwitchPin);
+  baroOn = digitalRead(baroSwitchPin);
+  id1On = digitalRead(id1SwitchPin);
+  id2On = digitalRead(id2SwitchPin);
+  id3On = digitalRead(id3SwitchPin);
+  id4On = digitalRead(id4SwitchPin);
+  pullOn = digitalRead(pullBeforeFlightPin);
+}
---
+++ PTERODACTYL/Sensors.ino
@@ -0,0 +1,219 @@
+#include <UbloxGPS.h>
+#include <TinyGPS++.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <SparkFunLSM9DS1.h>
+#include <OneWire.h>
+//#include <DallasTemperature.h>
+#include <MS5611.h>
+#include <SFE_MicroOLED.h>  // Include the SFE_MicroOLED library
+
+//The library assumes a reset pin is necessary. The Qwiic OLED has RST hard-wired, so pick an arbitrarty IO pin that is not being used
+#define PIN_RESET 9  
+//The DC_JUMPER is the I2C Address Select jumper. Set to 1 if the jumper is open (Default), or set to 0 if it's closed.
+#define DC_JUMPER 1 
+
+#define pressureOnePin A13         // Data pin for the first honeywell pressure sensor -- PCB pin:
+#define thermIntPin A16
+#define thermExtPin A17
+#define ubloxSerial Serial3       // Serial communication lines for the ublox GPS -- PCB pins: Serial5
+
+MS5611 baro;
+MicroOLED oled(PIN_RESET, DC_JUMPER);    // I2C declaration
+LSM9DS1 imu;
+UbloxGPS ublox(&ubloxSerial);
+
+/////////////// Thermistor constants //////////////////////
+
+float adcMax = pow(2,analogResolutionBits)-1.0; // The maximum adc value given to the thermistor
+float A = 0.001125308852122;
+float B = 0.000234711863267;
+float C = 0.000000085663516; // A, B, and C are constants used for a 10k resistor and 10k thermistor for the steinhart-hart equation
+float R1 = 10000; // 10k Ω resistor
+float Tinv;
+float adcVal;
+float logR;
+float T; // these three variables are used for the calculation from adc value to temperature
+float currentTempC; // The current temperature in Celcius
+float currentTempF; // The current temperature in Fahrenheit
+
+void ubloxSetup(){
+  ubloxSerial.begin(UBLOX_BAUD);
+  ublox.init();
+  
+  byte i = 0;
+  while (i<50) {
+    i++;
+    if (ublox.setAirborne()) {
+      Serial.println("Air mode successfully set.");
+      break;}
+    if (i==50){
+      Serial.println("Failed to set to air mode.");
+      updateOled("Failed to set GPS Air Mode");
+      delay(5000);
+    }
+  }
+  updateOled("GPS init\ncomplete!");
+  delay(1000);
+}
+
+void imuSetup(){
+  Wire.begin();
+  if(!imu.begin()){
+    Serial.println("Failed to communicate with LSM9DS1.");
+    updateOled("IMU\nOffline.");
+    delay(5000);
+  }
+  else{
+    updateOled("IMU init\ncomplete!");
+    delay(1000);
+  }
+}
+
+void updateIMU(){
+  if( imu.gyroAvailable() ) imu.readGyro();
+  if( imu.accelAvailable() ) imu.readAccel();
+  if( imu.magAvailable() ) imu.readMag();
+
+  magnetometer[0] = imu.calcMag(imu.mx);
+  magnetometer[1] = imu.calcMag(imu.my);
+  magnetometer[2] = imu.calcMag(imu.mz);
+  accelerometer[0] = imu.calcAccel(imu.ax);
+  accelerometer[1] = imu.calcAccel(imu.ay);
+  accelerometer[2] = imu.calcAccel(imu.az);
+  gyroscope[0] = imu.calcGyro(imu.gx);
+  gyroscope[1] = imu.calcGyro(imu.gy);
+  gyroscope[2] = imu.calcGyro(imu.gz);
+}
+
+void oledSetup(){
+  Wire.begin();
+  oled.begin();    // Initialize the OLED
+  oled.clear(ALL); // Clear the display's internal memory
+  oled.display();  // Display what's in the buffer (splashscreen)
+  //delay(1000);     // Delay 1000 ms
+  oled.clear(PAGE); // Clear the buffer.
+
+  randomSeed(analogRead(A0) + analogRead(A1));
+
+  updateOled("Initializing...");
+}
+
+void updateOled(String disp){
+  oled.clear(PAGE);
+  oled.setFontType(0);
+  oled.setCursor(0, 0);
+  oled.println(disp);
+  oled.display();
+}
+
+void msSetup() {
+  //updateOled("initializing\nbaro...");
+  while(!baro.begin()){
+    updateOled("baro init failed!");
+  }
+  /*if(!baro.begin()){
+    Serial.println("MS5611 Altimeter not active");
+    updateOled("digital baro not active");
+  }*/
+  updateOled("baro init\ncomplete!");
+  delay(1000);
+}
+
+void updateMS() {
+  msTemperature = baro.readTemperature();
+  msTemperature = msTemperature*(9.0/5.0) + 32.0;
+  msPressure = baro.readPressure(); 
+  msPressure = msPressure * 0.000145038;
+}
+
+void updatePressure() { // Output units: psi -- far from efficient, but works for our purpose
+  analogReadResolution(analogResolutionBits);
+  float rawPressure = analogRead(pressureOnePin);
+  float pressureVoltage = rawPressure*(3.3/analogResolutionVals);
+  float pressure = ((pressureVoltage - 0.33)*(15.0/2.66667));
+  pressureOnePSI = pressure;
+}
+
+void updateThermistor(){
+  analogReadResolution(analogResolutionBits);
+  adcVal = analogRead(thermIntPin);
+  logR = log(((adcMax/adcVal)-1)*R1);
+  Tinv = A+B*logR+C*logR*logR*logR;
+  T = 1/Tinv;
+  currentTempC = T-273.15; // converting to celcius
+  currentTempF = currentTempC*9/5+32;
+  thermistorInt = currentTempF;
+
+  adcVal = analogRead(thermExtPin);
+  logR = log(((adcMax/adcVal)-1)*R1);
+  Tinv = A+B*logR+C*logR*logR*logR;
+  T = 1/Tinv;
+  currentTempC = T-273.15; // converting to celcius
+  currentTempF = currentTempC*9/5+32;
+  thermistorExt = currentTempF;
+}
+
+void updateUblox(){
+  ublox.update();
+}
+
+void updateDataStrings(){
+  altitudeFtGPS = ublox.getAlt_feet();
+  latitudeGPS = ublox.getLat();
+  longitudeGPS = ublox.getLon();
+
+ groundData = String(ublox.getMonth()) + "/" + String(ublox.getDay()) + "/" + String(ublox.getYear()) + "," +
+            String(ublox.getHour()-5) + ":" + String(ublox.getMinute()) + ":" + String(ublox.getSecond()) + ","
+           + String(ublox.getLat(), 4) + ", " + String(ublox.getLon(), 4) + ", " + String(altitudeFtGPS, 4)
+           +  ", " + String(altitudeFt) + ", " + String(thermistorInt) + ", " + String(thermistorExt) + ", "
+           + String(msTemperature) + ", " + String(pressureOnePSI) + ", " + String(msPressure) + ", " + String(millis()/1000.0) + ", ";
+
+ data = groundData + String(magnetometer[0]) + ", " + String(magnetometer[1]) + ", " + String(magnetometer[2]) + ", " +
+            String(accelerometer[0]) + ", " + String(accelerometer[1]) + ", " + String(accelerometer[2]) + ", " +
+            String(gyroscope[0]) + ", " + String(gyroscope[1]) + ", " + String(gyroscope[2]) + "," +  xbeeMessage;
+
+ if(ppod==0) data = data + ", " + String(smartRelease);
+ 
+  updateOled(String(latitudeGPS) + "\n" + String(longitudeGPS) + "\n" + String(altitudeFtGPS,1) + "ft\nInt:" + String(int(thermistorInt)) + " F\nExt:" + String(int(thermistorExt)) + " F\n" + String(msPressure,2) + " PSI");
+  if(ublox.getFixAge() > 2000) fix = false;
+  else fix = true;
+  logData(data);
+}
+
+void pullPin(){
+  updateOled("Pull Flight Pin to start timer");
+  
+  while(pullOn==0)
+  {
+    digitalWrite(fixLED,HIGH);
+    digitalWrite(ppodLED,HIGH);
+    digitalWrite(xbeeLED,HIGH);
+    digitalWrite(sdLED,HIGH);
+    pullOn = digitalRead(pullBeforeFlightPin);
+  }
+  ppodOffset = millis();
+  updateOled("Timer Starting");
+  digitalWrite(fixLED,LOW);
+  digitalWrite(ppodLED,LOW);
+  digitalWrite(xbeeLED,LOW);
+  digitalWrite(sdLED,LOW);
+  delay(2000);
+}
+
+void ledGlissando() {
+  digitalWrite(fixLED,HIGH);
+  delay(50);
+  digitalWrite(ppodLED,HIGH);
+  delay(50);
+  digitalWrite(fixLED,LOW);
+  digitalWrite(xbeeLED,HIGH);
+  delay(50);
+  digitalWrite(ppodLED,LOW);
+  digitalWrite(sdLED,HIGH);
+  delay(50);
+  digitalWrite(xbeeLED,LOW);
+  delay(50);
+  digitalWrite(sdLED,LOW);
+  delay(150);
+}
